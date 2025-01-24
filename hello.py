8414cd0035cb3b493bbba4e3bce9f03a2191ad21
David <cdavidb82@outlook.com>
2025-01-24 17:41:01 -0500

    Added logging

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    logging.info('Hello World!')
     return "Hello World!"
-
